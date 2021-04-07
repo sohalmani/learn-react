ad6be15a4a8a553a2a1ffd32e34fbbe197806c65
manisohal837 <manisohal837@gmail.com>
2021-04-07 18:32:22 +0530

    use 'useState' hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
+/**
+ * All hooks start from 'use' keyword.
+ * useState being the most used.
+ */
+import { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 function App() {
+  /**
+   * useState returns an array, containing:
+   * the state itself;
+   * and a function to change that state.
+   */
+  const [personState, setPersonState] = useState({
+    persons: [
+      { name: 'Mani', age: '21' },
+      { name: 'Chander', age: '24' },
+      { name: 'Muskaan', age: '20' },
+    ],
+    otherState: 'some other value',
+  });
+
+  console.log(personState);
+
+  const switchNameHandler = () => {
+    /**
+     * Don't do this 👇
+     *
+     * personState.persons[0].name = "Maninder";
+     *
+     * 👆 This is going to throw error. Use react
+     * setState method instead to change state.
+     */
+    setPersonState({
+      persons: [
+        { name: 'Maninder', age: '22' },
+        { name: 'Chander', age: '24' },
+        { name: 'Muskaan', age: '21' },
+      ],
+    });
+
+    console.log(personState);
+  };
   return (
     <div className="App">
       <h1>Hi, this is my beginning in learning react</h1>
       <p>This is really exciting...</p>
-      <Person name="Mani" age="21" />
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      />
       {/* 
         Properties passed as attributes are accessible
         within components itself.
       */}
-      <Person name="Chander" age="24">
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      >
         Chander loves to exercise.
       </Person>
-      <Person name="Muskaan" age="20" />
+      <Person
+        name={personState.persons[2].name}
+        age={personState.persons[2].age}
+      />
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,5 @@
 function Person(props) {
   return (
-    /*
-     *  Properties are accessible inside props object,
-     *  passed as argument to component. Anything written
-     *  inside the component is accessible as children of prop.
-     */
     <div>
       <p>
         This is {props.name} and the age is {props.age}.
