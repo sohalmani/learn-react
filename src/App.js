cbfea873ccbd791ae778f75a108f85c4843f8f50
manisohal837 <manisohal837@gmail.com>
2021-04-07 17:36:54 +0530

    working with props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
     <div className="App">
       <h1>Hi, this is my beginning in learning react</h1>
       <p>This is really exciting...</p>
-      <Person />
+      <Person name="Mani" age="21" />
+      {/* 
+        Properties passed as attributes are accessible
+        within components itself.
+      */}
+      <Person name="Chander" age="24">
+        Chander loves to exercise.
+      </Person>
+      <Person name="Muskaan" age="20" />
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,17 @@
-function Person() {
-  return <p>This is a Person!</p>;
+function Person(props) {
+  return (
+    /*
+     *  Properties are accessible inside props object,
+     *  passed as argument to component. Anything written
+     *  inside the component is accessible as children of prop.
+     */
+    <div>
+      <p>
+        This is {props.name} and the age is {props.age}.
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 }
 
 export default Person;
