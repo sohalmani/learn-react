8c38631df045b314a47aacbb57c613d25f37afba
manisohal837 <manisohal837@gmail.com>
2021-04-07 11:13:03 +0530

    understand jsx restriction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, this is my beginning in learning react</h1>
+      <p>This is really exciting...</p>
     </div>
+    // React components cannot have adjacent elements. 
+    // Everything should be inside one root element.
   );
 }
 
