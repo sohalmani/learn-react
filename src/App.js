3d6fa443e1f99995e9d70e3ed1a6c5abefc71919
manisohal837 <manisohal837@gmail.com>
2021-04-07 18:46:45 +0530

    manage multiple state with useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-/**
- * All hooks start from 'use' keyword.
- * useState being the most used.
- */
 import { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 function App() {
-  /**
-   * useState returns an array, containing:
-   * the state itself;
-   * and a function to change that state.
-   */
   const [personState, setPersonState] = useState({
     persons: [
       { name: 'Mani', age: '21' },
@@ -21,17 +12,13 @@ function App() {
     otherState: 'some other value',
   });
 
+  /**
+   * this logs the person state with person array
+   * as well as other state.
+   */
   console.log(personState);
 
   const switchNameHandler = () => {
-    /**
-     * Don't do this 👇
-     *
-     * personState.persons[0].name = "Maninder";
-     *
-     * 👆 This is going to throw error. Use react
-     * setState method instead to change state.
-     */
     setPersonState({
       persons: [
         { name: 'Maninder', age: '22' },
@@ -40,8 +27,35 @@ function App() {
       ],
     });
 
+    /**
+     * this logs just the person state with person array,
+     * other value is not logged.
+     * This is because 'setState' in 'useState' hook replaces
+     * the state that is defined again while using setState.
+     *
+     * To overcome this, you can:
+     * 1) set otherValue to same value again from previous state; like
+      setPersonState({
+        persons: [
+          { name: 'Maninder', age: '22' },
+          { name: 'Chander', age: '24' },
+          { name: 'Muskaan', age: '21' },
+        ],
+        otherState: personState.otherState
+      });
+     * 2) Or use the 'useState' again to define otherValue.
+     */
     console.log(personState);
   };
+
+  /**
+   * We will use 'useState' again, this is much more effecient.
+   */
+  // eslint-disable-next-line
+  const [otherState, setOtherState] = useState('some new value');
+
+  console.log(personState, otherState);
+
   return (
     <div className="App">
       <h1>Hi, this is my beginning in learning react</h1>
@@ -51,10 +65,6 @@ function App() {
         name={personState.persons[0].name}
         age={personState.persons[0].age}
       />
-      {/* 
-        Properties passed as attributes are accessible
-        within components itself.
-      */}
       <Person
         name={personState.persons[1].name}
         age={personState.persons[1].age}
