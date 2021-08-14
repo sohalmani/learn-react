f3b80b5aaf5fdb11104e57f01caa3ee58bcef84b
manisohal837 <manisohal837@gmail.com>
2021-08-14 12:28:21 +0530

    make new state for other value

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ function App() {
       { name: 'Chander', age: '24' },
       { name: 'Muskaan', age: '20' },
     ],
-    otherState: 'some other value',
   });
 
-  /**
-   * this logs the person state with person array
-   * as well as other state.
-   */
-  console.log(personState);
+  // eslint-disable-next-line
+  const [otherState, setOtherState] = useState('some new value');
 
   const switchNameHandler = () => {
     setPersonState({
@@ -26,39 +22,11 @@ function App() {
         { name: 'Muskaan', age: '21' },
       ],
     });
-
-    /**
-     * this logs just the person state with person array,
-     * other value is not logged.
-     * This is because 'setState' in 'useState' hook replaces
-     * the state that is defined again while using setState.
-     *
-     * To overcome this, you can:
-     * 1) set otherValue to same value again from previous state; like
-      setPersonState({
-        persons: [
-          { name: 'Maninder', age: '22' },
-          { name: 'Chander', age: '24' },
-          { name: 'Muskaan', age: '21' },
-        ],
-        otherState: personState.otherState
-      });
-     * 2) Or use the 'useState' again to define otherValue.
-     */
-    console.log(personState);
   };
-
-  /**
-   * We will use 'useState' again, this is much more effecient.
-   */
-  // eslint-disable-next-line
-  const [otherState, setOtherState] = useState('some new value');
-
-  console.log(personState, otherState);
 
   return (
     <div className="App">
-      <h1>Hi, this is my beginning in learning react</h1>
+      <h1>Hi, this is my beginning in learning react.</h1>
       <p>This is really exciting...</p>
       <button onClick={switchNameHandler}>Switch name</button>
       <Person
