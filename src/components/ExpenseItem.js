da2f6a607827f88ff4a7d5a209027a48ad138119
manisohal837 <manisohal837@gmail.com>
2021-08-15 16:33:31 +0530

    add styling to custom component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
-      <h3>Expense Item!</h3>
+    <div className="expense-item">
+      <div>August 15th 2021</div>
+      <div className="expense-item__description">
+        <h2>Expense Item!</h2>
+        <div className="expense-item__price">$297.50</div>
+      </div>
     </div>
   );
 }
