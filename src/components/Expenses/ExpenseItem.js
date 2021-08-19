1eaf020edf2b5953efff04e754af083310097123
manisohal837 <manisohal837@gmail.com>
2021-08-19 20:42:02 +0530

    an effort to change title on click

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,22 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log('clicked!');
+    title = 'Updated';
+    console.log(title);
   };
+
+  // Title does not update in above way
+  // as we just change title value but
+  // the component does not re-render.
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Click Title</button>
