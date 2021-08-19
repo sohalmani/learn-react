75d0deacaf6e376e0c4e14f3133fb7c001d312fb
manisohal837 <manisohal837@gmail.com>
2021-08-19 20:19:27 +0530

    apply click events

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -8,7 +8,7 @@ const ExpenseDate = (props) => {
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
-      <div classname="expense-date__day">{day}</div>
+      <div className="expense-date__day">{day}</div>
       <div className="expense-date__year">{year}</div>
     </div>
   );
