a3220a98a53fa0a9c0fc10c5db4d2d214c429bd0
manisohal837 <manisohal837@gmail.com>
2022-06-01 09:28:05 +0530

    using stateful list for creating and updating expenses data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,12 +4,7 @@ import './Expenses.css';
 
 const Expenses = (props) => {
   const expenses = props.items;
-
-  // Instead of printing items one by one, 
-  // call a map function and return the items in an Array. 
-  // React can even render components in form of array, 
-  // like {[<ExpenseItem />, <ExpenseItem />, <ExpenseItem />]}
-  // which is same as calling <ExpenseItem /> in JSX itself.
+  
   return (
     <Card className="expenses">
       {expenses && expenses.map((expense) => (
