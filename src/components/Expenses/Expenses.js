70eed23fd33505488c10a0a6ecc8eab27147f4d1
manisohal837 <manisohal837@gmail.com>
2022-06-15 11:06:21 +0530

    understanding keys and implementation

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,10 +5,14 @@ import './Expenses.css';
 const Expenses = (props) => {
   const expenses = props.items;
   
+  // Add "key" so that React uniquely identifies 
+  // the component and only updates that instead of 
+  // updating all components.
   return (
     <Card className="expenses">
       {expenses && expenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
