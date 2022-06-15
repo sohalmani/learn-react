b0f59244dbe43efa3967136f7b7febb6bd60f7f3
manisohal837 <manisohal837@gmail.com>
2022-06-15 14:45:40 +0530

    conditionally return the expense list based on expenses in different component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -17,26 +17,11 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear
   });
 
-  // Store content message for no expenses
-  let expensesContent = <p>No expenses found!</p>;
-
-  // If there are expenses, store all of them 
-  // in a variable and output in JSX
-  if (expenses.length > 0) {
-    expensesContent = expenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
+  // add expenses list as different component
   return (
     <Card className="expenses">
       <ExpensesFilter selectedYear={filteredYear} onFilterChange={handleFilterChange} />
-      {expensesContent}
+      <ExpensesList items={expenses} />
     </Card>
   );
 };
