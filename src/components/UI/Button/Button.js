13a50144320b262496648fd50a21616f9559865b
manisohal837 <manisohal837@gmail.com>
2022-06-22 15:27:58 +0530

    add dynamic styles using CSS modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,6 @@
 import React from 'react';
-
-// Below is syntax for using CSS modules.
 import styles from './Button.module.css';
 
-// Use the impoted style in className. 
-// All CSS styles are properties of styles object
 const Button = props => {
   return (
     <button type={props.type} className={styles.button} onClick={props.onClick}>
@@ -12,6 +8,5 @@ const Button = props => {
     </button>
   );
 };
-
 
 export default Button;
