939a24d31ec4409364f397aaecc1d9a642de6d46
manisohal837 <manisohal837@gmail.com>
2022-06-22 10:44:22 +0530

    add css class conditionally and dynamically for invalid input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,43 +5,31 @@ import './CourseInput.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
-
-  // Invoke state for managing validation
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
     setIsValid(true);
-
-    // Set valid if user inputs
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
-    // Set valid if no value, prevent adding goal
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
-    
+
     props.onAddGoal(enteredValue);
   };
 
-
-  // Add styles conditionally based on if the user input is valid
+  // If invalid, add invalid class to form-control, 
+  // and invalid styles will appear
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input
-          type="text"
-          style={{
-            borderColor: !isValid ? 'red' : '#ccc',
-            backgroundColor: !isValid ? 'salmon' : 'transparent',
-          }}
-          onChange={goalInputChangeHandler}
-        />
+      <div className={`form-control ${!isValid ? 'invalid': ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
