cc997152bd6523494924507459db47baddb21cb9
manisohal837 <manisohal837@gmail.com>
2022-06-22 14:40:41 +0530

    add props dynamically to styled components by passing value thorugh component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,44 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+// style form-control using styled component
+// Keep note: props are passed by default/automatically to
+// components that are styled using styled components
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background-color: salmon;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -23,14 +60,15 @@ const CourseInput = (props) => {
     props.onAddGoal(enteredValue);
   };
 
-  // If invalid, add invalid class to form-control, 
-  // and invalid styles will appear
+  // In below case, passing className to FormControl 
+  // will auomatically pass 'className' props to the div. 
+  // It will assign value based on the condition.
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid': ''}`}>
+      <FormControl className={!isValid && 'invalid'}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
