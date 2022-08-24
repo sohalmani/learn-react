d7b52a4a6cc154cbdb00f4f3da116f4b40d65361
manisohal837 <manisohal837@gmail.com>
2022-08-25 00:57:21 +0530

    add users list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import AddUser from './components/AddUser/AddUser';
+import UsersList from './components/AddUser/UsersList';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList />
     </div>
   );
 }
