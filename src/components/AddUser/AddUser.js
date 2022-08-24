0bea0639e6e480646fe166e3684838d047d1ced4
manisohal837 <manisohal837@gmail.com>
2022-08-25 00:40:18 +0530

    validate user input

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -20,6 +20,14 @@ const AddUser = (props) => {
   const handleAddUserSubmit = (event) => {
     event.preventDefault();
 
+    if (enteredUsername.trim().length === 0 && enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+
     console.log(enteredUsername, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
