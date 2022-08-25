1e856392ee7ce02a8eb22c4a8dc1e8ebd32ba1e6
manisohal837 <manisohal837@gmail.com>
2022-08-25 23:00:17 +0530

    port ErrorModal outside 'root' element using createPortal method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, Fragment } from 'react';
+import { createPortal } from 'react-dom';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
@@ -29,17 +30,32 @@ function App() {
     setError(null);
   };
 
+  /**
+   * createPortal is an inbuilt functtion available in React-DOM.
+   * createPortal helps user to port/move any JSX component to 
+   * any particular place inside the HTML DOM.
+   * In below example, we displayed the ErrorModal outside the "root"
+   * div, to a new different div called "backdrop-root", that we
+   * created ourselves in index.html
+   */
+
   return (
     <Fragment>
       <AddUser onAddUser={handleUserAdd} onError={handleError} />
-      {!!usersList.length && <UsersList key="users-list-card" users={usersList} />}
-      {error && (
-        <ErrorModal
-          key="error-modal"
-          title={error.title}
-          message={error.message}
-          onConfirm={removeErrorModal}
-        />)}
+      {!!usersList.length && (
+        <UsersList key="users-list-card" users={usersList} />
+      )}
+      {createPortal(
+        error && (
+          <ErrorModal
+            key="error-modal"
+            title={error.title}
+            message={error.message}
+            onConfirm={removeErrorModal}
+          />
+        ),
+        document.getElementById('backdrop-root')
+      )}
     </Fragment>
   );
 }
